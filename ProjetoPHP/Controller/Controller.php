bda2deee33e03ebbad1075a9e4ff04b08bed4378
Adautoo10 <adauto.facin05@gmail.com>
2022-10-04 15:47:29 -0300

    Alterando Controller

--- ProjetoPHP/Controller/Controller.php
+++ ProjetoPHP/Controller/Controller.php
@@ -15,4 +15,11 @@ abstract class Controller
         else
             exit('Arquivo da View não encontrado. Arquivo: ' . $view);
     }    
+
+
+    protected static function isAuthenticated()
+    {
+    if(!isset($_SESSION['usuario_logado']))
+        header("location: /login");
+    } 
 }
