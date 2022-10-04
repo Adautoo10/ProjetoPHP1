f84ac63f5acfac9e8e8e7ca61c771ef7871da651
Adautoo10 <adauto.facin05@gmail.com>
2022-10-04 15:45:50 -0300

    Alterando Banco

--- ProjetoPHP/Banco de Dados/fisico.sql
+++ ProjetoPHP/Banco de Dados/fisico.sql
@@ -9,6 +9,17 @@ create table pessoa(
    cpf char(11) not null,
    data_nascimento date not null,
    email varchar(255),
+create database db_sistema;
+
+use db_sistema;
+
+create table pessoa(
+   id int auto_increment not null,
+   nome varchar(45) not null,
+   rg varchar(45) not null,
+   cpf char(11) not null,
+   data_nascimento date not null,
+   email varchar(255),
    telefone varchar(11),
    endereco varchar(255),
    primary key (id)
@@ -36,4 +47,9 @@ create table funcionario(
     primary key (id)
 );
 
-
+create table usuario(
+	id int auto_increment not null,
+	email varchar(100) not null,
+	senha varchar(100) not null,
+    primary key (id)
+);
