cbf437013f88e1c5ce2462fa54bb5a9250632701
Adautoo10 <adauto.facin05@gmail.com>
2022-10-17 09:04:33 -0300

    Arrumando banco

--- ProjetoPHP/Banco de Dados/fisico.sql
+++ ProjetoPHP/Banco de Dados/fisico.sql
@@ -9,17 +9,6 @@ create table pessoa(
    cpf char(11) not null,
    data_nascimento date not null,
    email varchar(255),
-create database db_sistema;
-
-use db_sistema;
-
-create table pessoa(
-   id int auto_increment not null,
-   nome varchar(45) not null,
-   rg varchar(45) not null,
-   cpf char(11) not null,
-   data_nascimento date not null,
-   email varchar(255),
    telefone varchar(11),
    endereco varchar(255),
    primary key (id)
